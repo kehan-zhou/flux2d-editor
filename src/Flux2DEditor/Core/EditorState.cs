be20c419f7e2cf0156951153635132bab8f0b37d
Kehan Zhou <kehan.z@hotmail.com>
2025-04-29 14:43:18 +0800

    feat(core): add basic EditorState singleton for global editor status

---
+++ src/Flux2DEditor/Core/EditorState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flux2DEditor.Core
+{
+    public class EditorState
+    {
+        // Singleton instance
+        private static EditorState? _instance;
+        
+        public static EditorState? Instance => _instance ?? (_instance = new EditorState());
+
+        private EditorState() { }
+
+        public bool IsModified { get; set; }
+        public string? CurrentFilePath { get; set; }
+    }
+}
