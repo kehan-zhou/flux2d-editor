6408ac400d05b0f1ce5a1b1e1be98d776d4c6faa
Kehan Zhou <kehan.z@hotmail.com>
2025-04-29 16:08:23 +0800

    feat(core): add RectangleObject with drawing, selection, and interaction logic

---
+++ src/Flux2DEditor/Core/RectangleObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flux2DEditor.Core
+{
+    public class RectangleObject : SelectableObject
+    {
+        public RectangleF Bounds { get; private set; }
+
+        public RectangleObject(RectangleF bounds)
+        {
+            Bounds = bounds;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            using var pen = new Pen(IsSelected ? Color.Blue : Color.Black, 2);
+            g.DrawRectangle(pen, Bounds.X, Bounds.Y, Bounds.Width, Bounds.Height);
+        }
+
+        public override bool HitTest(PointF point)
+        {
+            return Bounds.Contains(point);
+        }
+
+        public override void Move(PointF offset)
+        {
+            Bounds = new RectangleF(
+                Bounds.X + offset.X,
+                Bounds.Y + offset.Y,
+                Bounds.Width,
+                Bounds.Height
+            );
+        }
+    }
+}
---
+++ src/Flux2DEditor/Core/SelectableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flux2DEditor.Core
+{
+    public abstract class SelectableObject
+    {
+        public bool IsSelected { get; set; } = false;
+
+        public abstract void Draw(Graphics g);
+        public abstract bool HitTest(PointF point);
+        public abstract void Move(PointF offset);
+    }
+}
