996bc3b5e03887fdab818de079507f6e519b5792
Kehan Zhou <kehan.z@hotmail.com>
2025-05-06 15:17:05 +0800

    feat: add visual feedback for selected objects with dashed border and corner handles

--- src/Flux2DEditor/Core/RectangleObject.cs
+++ src/Flux2DEditor/Core/RectangleObject.cs
@@ -17,8 +17,38 @@ namespace Flux2DEditor.Core
 
         public override void Draw(Graphics g)
         {
-            using var pen = new Pen(IsSelected ? Color.Blue : Color.Black, 2);
-            g.DrawRectangle(pen, Bounds.X, Bounds.Y, Bounds.Width, Bounds.Height);
+            if (IsSelected)
+            {
+                using var dashedPen = new Pen(Color.Blue, 2)
+                {
+                    DashStyle = System.Drawing.Drawing2D.DashStyle.Dash
+                };
+                g.DrawRectangle(dashedPen, Bounds.X, Bounds.Y, Bounds.Width, Bounds.Height);
+
+                float size = 6f;
+                var handleBrush = Brushes.White;
+                var handlePen = Pens.Black;
+
+                PointF[] corners = new PointF[]
+                {
+                    new PointF(Bounds.Left, Bounds.Top),
+                    new PointF(Bounds.Right, Bounds.Top),
+                    new PointF(Bounds.Right, Bounds.Bottom),
+                    new PointF(Bounds.Left, Bounds.Bottom)
+                };
+
+                foreach (var corner in corners)
+                {
+                    var handleRect = new RectangleF(corner.X - size/2, corner.Y - size/2, size, size);
+                    g.FillRectangle(handleBrush, handleRect);
+                    g.DrawRectangle(handlePen, handleRect.X, handleRect.Y, handleRect.Width, handleRect.Height);
+                }
+            }
+            else
+            {
+                using var pen = new Pen(Color.Black, 2);
+                g.DrawRectangle(pen, Bounds.X, Bounds.Y, Bounds.Width, Bounds.Height);
+            }
         }
 
         public override bool HitTest(PointF point)
